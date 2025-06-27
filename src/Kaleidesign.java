8302ace53c78a1aab9b69d7a1a7e5e6f6510fd83
Shirley Xia <xshirl1027@gmail.com>
2025-06-27 15:28:15 -0400

    changes

--- src/Kaleidesign.java
+++ src/Kaleidesign.java
@@ -1,30 +1,14 @@
 import javax.swing.*;
-import java.awt.event.*;
-/**
 
+
+/**
  */
-public class Kaleidesign extends JPanel implements ActionListener {
+public class Kaleidesign extends JApplet {
    
    /**
 	 * 
 	 */
-   private static final long serialVersionUID = 1L;
-   // menubar
-   static JMenuBar mb;
-
-   // JMenu
-   static JMenu new_, pen_c, background_c, pen_style;
-
-   // Menu items
-   static JMenuItem m1, m2, m3, m4;
-
-   // create a frame
-   static JFrame f;
-
-   static JLabel l;
-
-   static Pattern content;
-   static JFrame window;
+	private static final long serialVersionUID = 1L;
 
 /**
     * The main routine opens a window that displays a drawing area
@@ -34,47 +18,10 @@ public class Kaleidesign extends JPanel implements ActionListener {
     * class as an applet.
     */
    public static void main(String[] args) {
-      window = new JFrame("Simple Paint");
-      l = new JLabel("basic pattern");
-      Kaleidesign m = new Kaleidesign();
-      // create a menubar
-      mb = new JMenuBar();
-
-      // create a menu
-      new_ = new JMenu("New");
-      pen_c = new JMenu("Pen Color");
-      pen_style = new JMenu("Pen Style");
-      background_c = new JMenu("Background Color");
-
-      // create menuitems
-      m1 = new JMenuItem("Grid Pattern");
-      m2 = new JMenuItem("Basic Pattern");
-      m3 = new JMenuItem("Chaos Pattern");
-      m4 = new JMenuItem("Blank Canvas");
-
-      // add ActionListener to menuItems
-      m1.addActionListener(m);
-      m2.addActionListener(m);
-      m3.addActionListener(m);
-      m4.addActionListener(m);
-
-      // add menu items to menu
-      new_.add(m1);
-      new_.add(m2);
-      new_.add(m3);
-      new_.add(m4);
-
-      // add menu to menu bar
-      mb.add(new_);
-      mb.add(pen_c);
-      mb.add(background_c);
-      mb.add(pen_style);
-      mb.add(l);
-      window.setJMenuBar(mb);
+      JFrame window = new JFrame("Simple Paint");
       window.setSize(1400,750);
-
-      //RadPattern content = new RadPattern(1400,750,12);
-      content = new RadPattern(1400,750,12);
+      RadPattern content = new RadPattern(1400,750,12);
+      //GridPattern content = new GridPattern(1400,750,200);
       //BlankCanvas content = new BlankCanvas(1400,750);
       //Pattern content = new Pattern(1400, 750);
       content.setPenSize(3);
@@ -82,21 +29,18 @@ public class Kaleidesign extends JPanel implements ActionListener {
       window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
       window.setVisible(true);
    }
-   public void actionPerformed(ActionEvent e)
-   {
-      String s = e.getActionCommand();
-
-      // set the label to the menuItem that is selected
-      l.setText(s);
-      //RadPattern content = new RadPattern(1400,750,12);
-      if(s=="Grid Pattern") content = new GridPattern(1400,750,200);
-      if(s=="Basic Pattern") content = new RadPattern(1400,750,12);
-      if(s=="Chaos Pattern") content = new ChaosPattern(1400,750);
-      if(s=="Blank Canvas") content = new Pattern(1400,750);
-      //BlankCanvas content = new BlankCanvas(1400,750);
-      //Pattern content = new Pattern(1400, 750);
-      content.setPenSize(3);
-      window.setContentPane(content);
+   
+   /**
+    * The init method of the applet simply creates a SimplePaintPanel and
+    * uses it as the content pane of the applet.  All the work is done
+    * by the SimplePaintPanel.
+    */
+   public void init() {
+    setContentPane( new RadPattern(1400,750,12) );
+    //setContentPane( new GridPattern(1400,750,200) );
+	  //setContentPane( new BlankCanvas(1400,750) );
+	  //setContentPane( new Pattern(1400,750) );
    }
+   
 
-}
+} // end class SimplePaint
